b3bac97a170284355990cabb91bc408bf3acd674
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-03 17:11:22 -0500

    Add interface to bridge

--- scripts/setup-lxc.sh
+++ scripts/setup-lxc.sh
@@ -8,3 +8,12 @@ then
     echo "Adding brdige"
     sudo brctl addbr br0
 fi
+
+echo "Adding interfaces"
+HAS_IFS=$(sudo brctl show | tail -n1 | cut -f6 | tr -d [[:space:]])
+IF="enp0s8"
+if [ -z "$HAS_IFS" ]
+then
+    printf "Adding %s to the bridge\n" "$IF"
+    sudo brctl addif br0 $IF
+fi
