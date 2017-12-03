00efd329d8691fb3279d45665818aa33cad51a89
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-03 17:04:19 -0500

    Adding bridge

--- scripts/setup-lxc.sh
+++ scripts/setup-lxc.sh
@@ -1,2 +1,10 @@
 echo "Installing lxc"
 sudo apt-get install -y lxc
+
+echo "Setup bridge"
+HAS_BRIDGE=$(ip a | grep br0)
+if [ -z "$HAS_BRIDGE" ]
+then
+    echo "Adding brdige"
+    sudo brctl addbr br0
+fi
