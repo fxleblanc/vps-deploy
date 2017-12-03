a437ef907d63c3bce3f8d1fa20a0399c0f4cb61c
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-03 16:59:14 -0500

    Use scp instead of rsync

--- test.sh
+++ test.sh
@@ -10,5 +10,5 @@ then
 fi
 
 # Deploy command
-rsync -rv -e "ssh -p 2222 -i ~/.vagrant.d/insecure_private_key -o 'IdentitiesOnly=yes'" scripts vagrant@127.0.0.1://home/vagrant/
+scp -r -P 2222 -i ~/.vagrant.d/insecure_private_key -o 'IdentitiesOnly=yes' scripts vagrant@127.0.0.1://home/vagrant/
 ssh -p 2222 -i ~/.vagrant.d/insecure_private_key -o 'IdentitiesOnly=yes' vagrant@127.0.0.1 'bash -s' < main.sh
