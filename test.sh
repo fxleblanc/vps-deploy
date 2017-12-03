91e5ac9497de8eddd30a5cbad063c4a723a92435
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-03 16:25:55 -0500

    clean option for test script

--- test.sh
+++ test.sh
@@ -2,4 +2,12 @@
 #
 # Test script
 #
+
+# Destroy and start vagrant machine
+if [ $1 == "clean" ]
+then
+    vagrant destroy -f && vagrant up
+fi
+
+# Deploy command
 ssh -p 2222 -i ~/.vagrant.d/insecure_private_key -o 'IdentitiesOnly=yes' vagrant@127.0.0.1 'bash -s' < main.sh
