c04d9e4cff919344d967e86e5268973cd35cb140
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-06 20:26:13 -0500

    Use lxc-net instead of creating a bridge

--- scripts/setup-lxc.sh
+++ scripts/setup-lxc.sh
@@ -1,19 +1,7 @@
 echo "Installing lxc"
 sudo apt-get install -y lxc
 
-echo "Setup bridge"
-HAS_BRIDGE=$(ip a | grep br0)
-if [ -z "$HAS_BRIDGE" ]
-then
-    echo "Adding brdige"
-    sudo brctl addbr br0
-fi
-
-echo "Adding interfaces"
-HAS_IFS=$(sudo brctl show | tail -n1 | cut -f6 | tr -d [[:space:]])
-IF="enp0s8"
-if [ -z "$HAS_IFS" ]
-then
-    printf "Adding %s to the bridge\n" "$IF"
-    sudo brctl addif br0 $IF
-fi
+echo "Setup lxc-net"
+sudo bash -c "cat > /etc/default/lxc-net" << EOF 
+USE_LXC_BRIDGE="true"
+EOF
