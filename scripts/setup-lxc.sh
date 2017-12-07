3a95c046a4cded753777d2ae4b855913935bea03
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-06 20:28:30 -0500

    Change default lxc configuration

--- scripts/setup-lxc.sh
+++ scripts/setup-lxc.sh
@@ -5,3 +5,11 @@ echo "Setup lxc-net"
 sudo bash -c "cat > /etc/default/lxc-net" << EOF 
 USE_LXC_BRIDGE="true"
 EOF
+
+echo "Change the default lxc configuration"
+sudo bash -c "cat > /etc/lxc/default.conf" << EOF 
+lxc.network.type = veth
+lxc.network.link = lxcbr0
+lxc.network.flags = up
+lxc.network.hwaddr = 00:16:3e:xx:xx:xx
+EOF
